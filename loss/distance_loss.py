d2c5a23fa78102327acb913f790d9b853a29cd69
Ruoxuan-Xu <61612205+Ruoxuan-Xu@users.noreply.github.com>
2024-08-16 18:59:39 +0800

    Update distance_loss.py

--- loss/distance_loss.py
+++ loss/distance_loss.py
@@ -6,9 +6,6 @@ class DistanceLoss(nn.Module):
         super(DistanceLoss, self).__init__()
 
     def forward(self, cam1, cam2):
-        """
-        计算两张灰度级CAM图像之间的欧氏距离，并返回负值作为损失
-        """
         # Flatten the CAM images to 1D tensors
         cam1_flat = cam1.view(-1)
         cam2_flat = cam2.view(-1)
